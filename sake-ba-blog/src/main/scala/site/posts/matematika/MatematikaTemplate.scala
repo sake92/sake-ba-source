1b38a28d11d9347bd217dbff936f2009d119e12b
sake92 <sake.etf@gmail.com>
2018-05-13 17:15:23 +0200

    Fixed compiler heisen-error

--- sake-ba-blog/src/main/scala/site/posts/matematika/MatematikaTemplate.scala
+++ sake-ba-blog/src/main/scala/site/posts/matematika/MatematikaTemplate.scala
@@ -1,13 +1,15 @@
 package site.posts.matematika
 
 import hepek.templates.SakeBaBlogPage
+import ba.sake.hepek.html.structure.blog.BlogPostPage
 import ba.sake.hepek.mathjax.MathjaxDependencies
 
 trait MatematikaTemplate extends SakeBaBlogPage with MathjaxDependencies {
 
   override def pageCategory = Option("Matematika")
 
-  override def categoryPosts = List(Index, TeorijaSkupova)
+  // compiler sometimes doesn't infer the type ??????????????
+  override def categoryPosts: List[BlogPostPage] = List(Index, TeorijaSkupova)
 
   // https://en.wikipedia.org/wiki/Acute_accent
   // couldn't find better escape character,
