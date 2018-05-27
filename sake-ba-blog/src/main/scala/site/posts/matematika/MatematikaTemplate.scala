1ae114f1ddc43a01309d8ca544f6e25e7975a57a
sake92 <sake.etf@gmail.com>
2018-05-28 00:51:07 +0200

    Removed redundant math config

--- sake-ba-blog/src/main/scala/site/posts/matematika/MatematikaTemplate.scala
+++ sake-ba-blog/src/main/scala/site/posts/matematika/MatematikaTemplate.scala
@@ -11,18 +11,4 @@ trait MatematikaTemplate extends SakeBaBlogPage with KatexDependencies {
   // compiler sometimes doesn't infer the type ??????????????
   override def categoryPosts: List[BlogPostPage] = List(Index, TeorijaSkupova)
 
-  // https://en.wikipedia.org/wiki/Acute_accent
-  // couldn't find better escape character, all other are used in Markdown
-  override def scriptsInline =
-    super.scriptsInline ++
-      List("""
-        renderMathInElement(
-          document.body, {
-            delimiters: [
-              { left: "´", right: "´", display: false }, // inline
-              { left: "$$", right: "$$", display: true } // block, centered
-            ]
-        });
-      """)
-
 }
